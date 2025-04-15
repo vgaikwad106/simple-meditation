ba9091d863c3ec9abe92a7c8fe05ad97e6b4c31e
Vaishnavi <uw.vgaikwad24@gmail.com>
2025-04-15 12:59:55 -0700

    Added assets, changed file structure, and created background

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, ImageBackground, Image } from "react-native";
+import React from "react";
+import beachImage from "../assets/meditation-images/beach.webp";
+
+const App = () => {
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1">
+        <Text>App</Text>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default App;
---
+++ image.d.ts
@@ -0,0 +1,31 @@
+//Declare different typescript rules
+
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.webp" {
+  const value: any;
+  export default value;
+}
